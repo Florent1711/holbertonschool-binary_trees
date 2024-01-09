2147fd98ea9cdefb6c9ab6f387b406caadd08a80
Florent1711 <graphit01@hotmail.com>
2024-01-09 06:01:42 -0800

    correction de Betty style

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_preorder -  goes through a binary tree using pre-order traversal
+ * @func: call function
+ * @tree: tree to go through
+ * Return: nothing
+ */
+
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
