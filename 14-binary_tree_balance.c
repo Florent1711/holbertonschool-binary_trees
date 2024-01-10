bb07e034cffc39e018c2b0200b923a0fd9e5e9a5
lchenevi <leo.cheneviere@gmail.com>
2024-01-10 14:26:49 +0100

    changing the name of a part of the first part of my function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,19 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Computes the height of a binary tree.
+ * _binary_tree_height - Computes the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
  *
  * Return: The height of the binary tree.
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t _binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	size_t left_height = _binary_tree_height(tree->left);
+	size_t right_height = _binary_tree_height(tree->right);
 
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
@@ -29,8 +29,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	size_t left_height = _binary_tree_height(tree->left);
+	size_t right_height = _binary_tree_height(tree->right);
 
 	return ((int)(left_height - right_height));
 }
