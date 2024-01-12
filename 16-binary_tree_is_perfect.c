21cd063a95761a3feae585c5b377f766bc01bb87
lchenevi <leo.cheneviere@gmail.com>
2024-01-12 11:53:02 +0100

    trying to solve the tiny problem to get full points

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,23 +1,30 @@
 #include "binary_trees.h"
 
 /**
- * _binary_tree_height - Computes the height of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the height.
- *
- * Return: The height of the binary tree.
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure the height
+ * Return: the height of a binary tree. If tree is NULL, returns 0
  */
-size_t _binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height;
-	size_t right_height;
 
-	if (tree == NULL)
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left_height = 0, right_height = 0;
+
+	/*If the tree is empty, return 0*/
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	left_height = _binary_tree_height(tree->left);
-	right_height = _binary_tree_height(tree->right);
+	/*Recursively compute the height of the left and right subtrees*/
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	/*Return the maximum height of the two subtrees plus 1 for the*/
+	/*current node*/
+	/* transformation de la ternaire */
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
 
 /**
@@ -34,8 +41,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = _binary_tree_height(tree->left);
-	right_height = _binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	/*Check if the tree is full and left and right heights are equal*/
 	if (binaire_tree_is_full(tree) && left_height == right_height)
